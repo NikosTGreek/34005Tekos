c790ec0f057f772cad52c8f1faa6028e26214055
Nikos <96896558+NikosTGreek@users.noreply.github.com>
2022-10-24 19:39:42 -0500

    Update game.js

--- game.js
+++ game.js
@@ -12,7 +12,7 @@ let availableQuestions = []
 
 let questions = [];
 
-fetch("/34005/questions.json")
+fetch("questions.json")
     .then(res => {
         return res.json();
     })
@@ -39,7 +39,7 @@ getNewQuestion = () => {
     if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score)
 
-        return window.location.assign('/34005Tekos/end.html')
+        return window.location.assign("/34005Tekos/end.html")
     }
 
     questionCounter++
